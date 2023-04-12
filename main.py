7c7b856272b007e268523db500609f13cf4402cd
Emre Çalışır <emrecalisir95@gmail.com>
2023-04-12 23:16:26 +0300

    Initialize

---
+++ main.py
@@ -0,0 +1,37 @@
+import openai
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+discord_bot_token = os.getenv("DISCORD_BOT_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+intents = discord.Intents.default()
+intents.members = True  # Subscribe to the "members" intent
+
+client = discord.Client(intents=intents)
+
+messages = [{"role": "system", "content": "You are ChatGPT."}]
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    messages.append({"role":"user",
+                     "content": message.content})
+
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages = messages
+    )
+
+    messages.append({"role":"assistant",
+                     "content": response.choices[0]["message"].content})
+
+    await message.channel.send(response.choices[0]["message"].content)
+
+client.run(discord_bot_token)
